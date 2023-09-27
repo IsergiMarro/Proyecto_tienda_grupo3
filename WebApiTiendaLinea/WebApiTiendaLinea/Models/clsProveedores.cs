56fd9f4a880788222bbcdb2bcb5b62404d35bb91
eterium115 <diegogramajo776@gmail.com>
2023-09-26 21:59:07 -0600

    Se agrega modelo proveedor

--- WebApiTiendaLinea/WebApiTiendaLinea/Models/clsProveedores.cs
+++ WebApiTiendaLinea/WebApiTiendaLinea/Models/clsProveedores.cs
@@ -2,5 +2,10 @@
 {
     public class clsProveedores
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tel { get; set; }
+        public string Direccion { get; set;}
+        public string Descripcion { get; set;}
     }
 }
